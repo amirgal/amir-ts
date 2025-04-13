628f8669d596de268221d32db2309dcd35a84978
Amir Gal <amir.gal@headstart.co.il>
2025-04-13 20:14:25 +0300

    add react-query and test prefetch

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,27 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
+const queryClient = new QueryClient()
+
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, context: { queryClient } })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
   }
+  interface RouterContext {
+    queryClient: QueryClient
+  }
 }
+
 
 // Render the app
 const rootElement = document.getElementById('root')!
@@ -22,7 +29,9 @@ if (!rootElement.innerHTML) {
   const root = createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
